8db34d4ca28a4b1f7431c1d24c1a9f02ccba20e7
Sergio Peralta <seperalta88@gmail.com>
2023-05-14 22:13:08 -0300

    Update login page

--- lib/src/login/login_page.dart
+++ lib/src/login/login_page.dart
@@ -16,57 +16,124 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
         body: Container(
       width: double.infinity,
-      child: Column(
-        children: [
-          imageBanner(),
-          TextField(
-            decoration: InputDecoration(
-                hintText: 'Email',
-                labelText: 'Email',
-                suffixIcon: const Icon(Icons.email),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10))),
-          ),
-          TextField(
-            decoration: InputDecoration(
-                hintText: 'Password',
-                labelText: 'Password',
-                suffixIcon: const Icon(Icons.lock),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10))),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-                backgroundColor: MyColors.primaryColor,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 50, vertical: 15)),
-            child: const Text('Sign in'),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('You don\'t have an account?'),
-              TextButton(
-                  onPressed: () {},
-                  child: const Text('Sign up',
-                      style: TextStyle(
-                          color: MyColors.primaryColor,
-                          fontWeight: FontWeight.bold)))
-            ],
-          )
-        ],
-      ),
+      child: Stack(children: [
+        Positioned(child: _circleLogin(), top: -150, left: -80),
+        Column(
+          children: [
+            _imageBanner(),
+            _textFieldEmail(),
+            const SizedBox(height: 10),
+            _textFieldPassword(),
+            const SizedBox(height: 20),
+            _buttonLogin(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('You don\'t have an account?'),
+                _textButtonSignUp()
+              ],
+            )
+          ],
+        ),
+      ]),
     ));
   }
 
-  Widget imageBanner() {
-    return Image.asset(
-      'assets/img/delivery.png',
-      height: 200,
-      width: 200,
+  Widget _circleLogin() {
+    return Container(
+        width: 250,
+        height: 250,
+        decoration: BoxDecoration(
+            color: MyColors.primaryColor,
+            borderRadius: BorderRadius.circular(80)),
+        alignment: Alignment.center,
+        child: const Padding(
+          padding: EdgeInsets.only(top: 130, left: 40),
+          child: Text(
+            'Login',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 35,
+            ),
+          ),
+        ));
+  }
+
+  Widget _imageBanner() {
+    return Container(
+      margin: EdgeInsets.only(
+          top: 100, bottom: MediaQuery.of(context).size.height * 0.09),
+      child: Image.asset(
+        'assets/img/delivery.png',
+        height: 200,
+        width: 200,
+      ),
     );
+  }
+
+  Widget _textFieldEmail() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+      decoration: BoxDecoration(
+        color: MyColors.primaryColorOpacity,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: const TextField(
+        decoration: InputDecoration(
+          hintText: 'Email',
+          labelText: 'Email',
+          labelStyle: TextStyle(color: MyColors.primaryColor),
+          border: OutlineInputBorder(borderSide: BorderSide.none),
+          contentPadding: EdgeInsets.all(20),
+          suffixIcon: Icon(Icons.email, color: MyColors.primaryColor),
+        ),
+      ),
+    );
+  }
+
+  Widget _textFieldPassword() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+      decoration: BoxDecoration(
+        color: MyColors.primaryColorOpacity,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: const TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          hintText: 'Password',
+          labelText: 'Password',
+          labelStyle: TextStyle(color: MyColors.primaryColor),
+          border: OutlineInputBorder(borderSide: BorderSide.none),
+          contentPadding: EdgeInsets.all(20),
+          suffixIcon: Icon(Icons.lock_open, color: MyColors.primaryColor),
+        ),
+      ),
+    );
+  }
+
+  Widget _buttonLogin() {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+            backgroundColor: MyColors.primaryColor,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 15)),
+        child: const Text('Sing In',
+            style: TextStyle(color: Colors.white, fontSize: 17)),
+      ),
+    );
+  }
+
+  Widget _textButtonSignUp() {
+    return TextButton(
+        onPressed: () {},
+        child: const Text('Sign up',
+            style: TextStyle(
+                color: MyColors.primaryColor, fontWeight: FontWeight.bold)));
   }
 }
--- lib/src/utils/my_colors.dart
+++ lib/src/utils/my_colors.dart
@@ -1,6 +1,10 @@
 import 'dart:ui';
 
 class MyColors {
-  static const Color primaryColor = Color(0xffbc112a);
+  static const Color primaryColor = Color(0xffe70D32);
+  static const Color primaryColorDark = Color(0xffa7001c);
+  static const Color primaryColorOpacity = Color.fromRGBO(231, 13, 50, 0.09);
+
+  //gray
   static const Color grayText = Color(0xff7f7f7f);
 }
